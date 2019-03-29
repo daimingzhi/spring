f319ba8c2c913833a3daf2416445bb5e44fd829f
daimzh <daimzh@creativearts.cn>
2019-03-29 22:11:50 +0800

    web

---
+++ http-message-converter/src/main/java/com/study/spring/httpmessageconverter/HttpMessageConverterApplication.java
@@ -0,0 +1,16 @@
+package com.study.spring.httpmessageconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * @author daimzh
+ */
+@SpringBootApplication
+public class HttpMessageConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpMessageConverterApplication.class, args);
+    }
+
+}
